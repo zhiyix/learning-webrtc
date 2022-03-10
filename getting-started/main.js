cb67b6db838a1686cbc5c47e1eea49f639de736e
王志毅 <wangzhiyi@hzyele.com>
2022-03-10 21:00:49 +0800

    getting-started ready

--- getting-started/main.js
+++ getting-started/main.js
@@ -1,29 +1,110 @@
 "use strict"
 
-const videoElement = document.querySelector('div#local video');
-const constrains = {
-	'video': true,
-	'audio': true
-}
+document.addEventListener('DOMContentLoaded', init);
 
-navigator.mediaDevices.getUserMedia(constrains)
-.then(stream => {
-	videoElement.srcObject = stream;
-	console.log('Got MediaStream:', stream)
-})
-.catch(error => {
-	console.log('Error accessing media devices.', error)
-})
+async function init() {
+  console.log('DOMContentLoaded');
+	const videoElement = document.querySelector('div#local video');
+	const constrains = {
+		'video': true,
+		'audio': true
+	}
 
-// 查询媒体设备
-function getConnectedDevices(type, callback) {
-	navigator.mediaDevices.enumerateDevices()
-	.then(devices => {
-		const filterd = devices.filter(device => device.kind === type);
-		callback(filterd)
+	navigator.mediaDevices.getUserMedia(constrains)
+	.then(stream => {
+		videoElement.srcObject = stream;
+		console.log('Got MediaStream:', stream)
 	})
-}
+	.catch(error => {
+		console.log('Error accessing media devices.', error)
+	})
 
-getConnectedDevices('videoinput', cameras => {
-	console.log('Cameras found ', cameras)
-})
+	// 查询媒体设备
+	function getConnectedDevices(type, callback) {
+		navigator.mediaDevices.enumerateDevices()
+		.then(devices => {
+			const filterd = devices.filter(device => device.kind === type);
+			callback(filterd)
+		})
+	}
+
+	getConnectedDevices('videoinput', cameras => {
+		console.log('Cameras found ', cameras)
+	})
+
+
+	const createPeerConnectionButton = document.querySelector('button#createPeerConnection');
+
+	createPeerConnectionButton.onclick = createPeerConnection;
+
+
+	let localPeerConnection;
+	let remotePeerConnection;
+	let localStream;
+
+	function createLocalPeerConnection() {
+		// 本地端
+		window.localPeerConnection = localPeerConnection = new RTCPeerConnection(servers);
+		console.log('Created local peer connection object localPeerConnection');
+		// Listen for local ICE candidates on the local RTCPeerConnection
+		localPeerConnection.onicecandidate = e => {
+			// onIceCandidate(localPeerConnection, e)
+			try {
+			// eslint-disable-next-line no-unused-vars
+			const ignore = remotePeerConnection.addIceCandidate(e.candidate);
+			console.log('AddIceCandidate success.');
+			} catch (e) {
+			console.log(`Failed to add Ice Candidate: ${e.toString()}`);
+			}
+			/* send
+			if (event.candidate) {
+				signalingChannel.send({'new-ice-candidate': event.candidate});
+			}*/
+
+			console.log(`localPeerConnection ICE candidate:\n${e.candidate ? e.candidate.candidate : '(null)'}`);
+		};
+		sendChannel = localPeerConnection.createDataChannel('sendDataChannel', dataChannelOptions);
+		sendChannel.onopen = onSendChannelStateChange;
+		sendChannel.onclose = onSendChannelStateChange;
+		sendChannel.onerror = onSendChannelStateChange;
+	}
+	
+	function createRemotePeerConnection() {
+		// 远程端
+		window.remotePeerConnection = remotePeerConnection = new RTCPeerConnection(servers);
+		console.log('Created remote peer connection object remotePeerConnection');
+		// Listen for remote ICE candidates on the remote RTCPeerConnection
+		remotePeerConnection.onicecandidate = e => {
+			// onIceCandidate(remotePeerConnection, e)
+			try {
+			// eslint-disable-next-line no-unused-vars
+			const ignore = localPeerConnection.addIceCandidate(e.candidate);
+			console.log('AddIceCandidate success.');
+			} catch (e) {
+			console.log(`Failed to add Ice Candidate: ${e.toString()}`);
+			}
+			/* send
+			if (event.candidate) {
+				signalingChannel.send({'new-ice-candidate': event.candidate});
+			}*/
+
+			console.log(`remotePeerConnection ICE candidate:\n${e.candidate ? e.candidate.candidate : '(null)'}`);
+		}; 
+		remotePeerConnection.ontrack = gotRemoteStream;
+		remotePeerConnection.ondatachannel = receiveChannelCallback;
+	}
+
+	function createPeerConnection() {
+		console.log('Starting call');
+		const videoTracks = localStream.getVideoTracks();
+		const audioTracks = localStream.getAudioTracks();
+
+		const servers = null;
+		createLocalPeerConnection();
+		createRemotePeerConnection();
+
+		localStream.getTracks()
+			.forEach(track => localPeerConnection.addTrack(track, localStream));
+		console.log('Adding Local Stream to peer connection');
+	}
+}
